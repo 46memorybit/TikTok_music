81f239f815e1beebd13ceb0bd683ac8814ab3cfa
46memorybit <secretgiants819@gmail.com>
2026-05-22 12:03:49 +0900

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -3,7 +3,7 @@ const { Client } = require("@notionhq/client");
 
 // ===== 対象音源URL（複数OK）=====
 const TARGET_URLS = [
-  "https://www.tiktok.com/music/Lonesome-rabbit-7637343996108834832?is_from_webapp=1&sender_device=pc"
+  "https://www.tiktok.com/music/Lonesome-rabbit-7637343996108834832?is_from_webapp=1&sender_device=pc",
   "https://www.tiktok.com/music/What's-KAZOKU-7626597456344647697?is_from_webapp=1&sender_device=pc",
   "https://www.tiktok.com/music/ドライフルーツ-7607761786352666640?is_from_webapp=1&sender_device=pc",
   "https://www.tiktok.com/music/光源-7605167475690653713?is_from_webapp=1&sender_device=pc",
